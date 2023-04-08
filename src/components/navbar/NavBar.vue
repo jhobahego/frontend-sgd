34e80513266993e089fb4e1d79c29ebc03e4c085
Jhon Hernandez <jhobahego16@hotmail.com>
2023-04-08 18:35:28 -0500

    navbar responsive y estilado

--- src/components/navbar/NavBar.vue
+++ src/components/navbar/NavBar.vue
@@ -1,30 +1,41 @@
 <template>
-  <div id="showNavegation">
-    <nav class="navbar">
+  <nav class="navbar" :class="{ 'navbar__opacity': isNavVisible }">
+    <div class="navbar__header">
       <router-link class="navbar__brand" to="/">Inicio</router-link>
-      <ul class="navbar__items">
-        <li class="navbar__item">
-          <router-link class="navbar__link" to="/documentos">Documentos</router-link>
-        </li>
-        <li class="navbar__item">
-          <router-link class="navbar__link" to="/about">Acerca de</router-link>
-        </li>
-        <li class="navbar__item">
-          <router-link class="navbar__link" to="#">Iniciar sesión</router-link>
-        </li>
+      <button class="navbar__toggle" @click="toggleNav">☰</button>
+    </div>
+    <ul class="navbar__items" :class="{ 'navbar__items--visible' : isNavVisible }">
+      <li class="navbar__item navbar__item--first">
+        <router-link class="navbar__link" to="/documentos">Documentos</router-link>
+      </li>
+      <li class="navbar__item">
+        <router-link class="navbar__link" to="/about">Acerca de</router-link>
+      </li>
+      <li class="navbar__item">
+        <router-link class="navbar__link" to="#">Iniciar sesión</router-link>
+      </li>
 
-        <li class="navbar__item">
-          <router-link class="navbar__link" to="#">Registrarse</router-link>
-        </li>
-      </ul>
-    </nav>
-  </div>
+      <li class="navbar__item">
+        <router-link class="navbar__link" to="#">Registrarse</router-link>
+      </li>
+    </ul>
+  </nav>
 </template>
 <script lang="ts">
 import { defineComponent } from 'vue';
 
 export default defineComponent({
   name: "showNavegation",
+  data() {
+    return {
+      isNavVisible: false
+    };
+  },
+  methods: {
+    toggleNav() {
+      this.isNavVisible = !this.isNavVisible;
+    }
+  }
 })
 </script>
 
