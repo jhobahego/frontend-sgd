b036afd81c0fa66b87a621eaa1edea8f76ec246a
Jhon Hernandez <jhobahego16@hotmail.com>
2023-04-08 20:18:26 -0500

    ruta de vista para comprar creada

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
 import HomeView from '../views/index/HomeView.vue'
 import AboutView from '../views/about/AboutView.vue'
 import DocumentsView from '../views/documents/DocumentsView.vue'
+import PurchaseView from '../views/documents/PurchaseView.vue'
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -18,7 +19,12 @@ const routes: Array<RouteRecordRaw> = [
     path: '/documentos',
     name: 'documentsView',
     component: DocumentsView
-  }
+  },
+  {
+    path: '/documentos/ :id',
+    name: 'purchaseView',
+    component: PurchaseView
+  },
 ]
 
 const router = createRouter({
