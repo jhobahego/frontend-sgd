1a4e99c829b1ba382ae565c4cd5cc31176eb54af
Jhon Hernandez <jhobahego16@hotmail.com>
2023-04-09 14:24:40 -0500

    contenedor y boton de compra para tarjeta detalle

--- src/views/documents/DocumentDetail.vue
+++ src/views/documents/DocumentDetail.vue
@@ -1,9 +1,10 @@
 <template>
-  <article class="document__card">  
+  <section class="document__container">
+    <article class="document__card">
       <figure class="img__container">
         <img class="document__img" src="@/assets/libro.jpeg" alt="libro the pragmatic programmer">
       </figure>
-      
+
       <div class="document__info">
         <h3 class="document__title">The pragmatic programmer</h3>
         <p class="document__description">
@@ -15,8 +16,11 @@
         <p class="document__description">
           stock: 23
         </p>
+        <button class="buy__btn">adquirir</button>
       </div>
+
     </article>
+  </section>
 </template>
 
 <script lang="ts">
