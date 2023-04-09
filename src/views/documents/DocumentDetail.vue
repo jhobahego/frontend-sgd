9338858aea5406cd0608c35edd955b53c1d5ca66
Jhon Hernandez <jhobahego16@hotmail.com>
2023-04-08 20:49:03 -0500

    vista de un documento en especifico

---
+++ src/views/documents/DocumentDetail.vue
@@ -0,0 +1,28 @@
+<template>
+  <article class="document__card">  
+      <figure class="img__container">
+        <img class="document__img" src="@/assets/libro.jpeg" alt="libro the pragmatic programmer">
+      </figure>
+      
+      <div class="document__info">
+        <h3 class="document__title">The pragmatic programmer</h3>
+        <p class="document__description">
+          From journey to mastery
+        </p>
+        <p class="document__description">
+          precio: 45$
+        </p>
+        <p class="document__description">
+          stock: 23
+        </p>
+      </div>
+    </article>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'documentDetalView',
+})
+</script>
