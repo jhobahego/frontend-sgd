bb30ffc0622f0610c1e34c14d1376a5a2027b5e4
Jhon Hernandez <jhobahego16@hotmail.com>
2023-04-10 20:40:59 -0500

    formulario de inicio sesión creado

---
+++ src/components/forms/auth/LoginForm.vue
@@ -0,0 +1,16 @@
+<template>
+  <form action="#" class="form__login">
+    <input class="login__input login__input--first" type="text" placeholder="correo electronico...">
+    <input class="login__input" type="password" placeholder="contraseña...">
+    <button class="btn__login">Iniciar sesión</button>
+    <span>¿olvidaste tu contraseña?</span>
+  </form>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: "loginForm",
+})
+</script>
