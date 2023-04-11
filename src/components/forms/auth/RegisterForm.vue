b7c1453fc8d04f50d09b75abb7532a84e62aad43
Jhon Hernandez <jhobahego16@hotmail.com>
2023-04-10 20:42:21 -0500

    formulario de registro creado

---
+++ src/components/forms/auth/RegisterForm.vue
@@ -0,0 +1,23 @@
+<template>
+  <form action="#" class="form__register">
+    <div class="register__div register__div--first">
+      <input class="register__input" type="text" placeholder="nombres...">
+      <input class="register__input" type="text" placeholder="apellidos...">
+    </div>
+    <input class="register__input" type="email" placeholder="correo electronico...">
+    <input class="register__input" type="password" placeholder="contraseña...">
+    <div class="register__div">
+      <input class="register__input" type="text" placeholder="ciudad...">
+      <input class="register__input" type="text" placeholder="país...">
+    </div>
+    <button class="btn__register">Registrarse</button>
+  </form>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: "registerForm",
+})
+</script>
