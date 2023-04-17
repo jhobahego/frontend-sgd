4e58ecdbf166c64a7faf59ec78dee5aa2092c257
Jhon Hernandez <jhobahego16@hotmail.com>
2023-04-17 14:20:16 -0500

    documentos cargados desde la api

--- src/views/documents/DocumentsView.vue
+++ src/views/documents/DocumentsView.vue
@@ -1,68 +1,67 @@
 <template>
   <section class="card__container">
-    <article class="card">
+    
+    <div v-for="documento in documentos" :key="documento._id">
+      <article class="card card--out" v-if="documento.stock < 1">
+      <h1 class="card__title">{{ documento.titulo }}</h1>
+      <div class="card-body">
+        <p class="card__description">Libro agotado pronto tendremos mas!!!</p>
+      </div>
+    </article>
+
+    <article class="card" v-else>
       <figure class="img__container">
         <img class="card__img" src="../../assets/libro.jpeg" alt="libro the pragmatic programmer" />
       </figure>
       <div class="card__body">
-        <h3 class="card__title">The pragmatic programmer</h3>
-        <p class="card__description">From journey to mastery</p>
-        <p class="card__description">Autor: Jhon hernandez</p>
-        <p class="card__description">Precio: 40$</p>
-        <p class="card__description">quedan 25 documentos</p>
+        <h3 class="card__title">{{documento.titulo}}</h3>
+        <p class="card__description">{{ documento.descripcion }}</p>
+        <p class="card__description">Autor: {{ documento.autor }}</p>
+        <p class="card__description">Precio: {{ documento.precio }}$</p>
+        <p class="card__description">quedan {{ documento.stock }} documentos</p>
 
         <button class="card__btn" @click.prevent="adquirir()">
           Adquirir
         </button>
       </div>
     </article>
+    </div>
 
-    <article class="card">
-      <figure class="img__container">
-        <img class="card__img" src="../../assets/libro.jpeg" alt="libro the pragmatic programmer" />
-      </figure>
-      <div class="card__body">
-        <h3 class="card__title">The pragmatic programmer</h3>
-        <p class="card__description">From journey to mastery</p>
-        <p class="card__description">Autor: Jhon hernandez</p>
-        <p class="card__description">Precio: 40$</p>
-        <p class="card__description">quedan 25 documentos</p>
-
-        <button class="card__btn" @click.prevent="adquirir()">
-          Adquirir
-        </button>
-      </div>
-    </article>
-
-    <article class="card">
-      <figure class="img__container">
-        <img class="card__img" src="../../assets/libro.jpeg" alt="libro the pragmatic programmer" />
-      </figure>
-      <div class="card__body">
-        <h3 class="card__title">The pragmatic programmer</h3>
-        <p class="card__description">From journey to mastery</p>
-        <p class="card__description">Autor: Jhon hernandez</p>
-        <p class="card__description">Precio: 40$</p>
-        <p class="card__description">quedan 25 documentos</p>
-
-        <button class="card__btn" @click.prevent="adquirir()">
-          Adquirir
-        </button>
-      </div>
-    </article>
   </section>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
+import { Document } from '@/interfaces/Document';
+import { obtenerDocumentos } from '@/services/DocumentService'
 
 export default defineComponent({
   name: "DocumentView",
+
+  data() {
+    return {
+      documentos: {} as Document[]
+    }
+  },
+  
   methods: {
+    async cargarDocumentos() {
+      const token = localStorage.getItem("token");
+      if(token === null) return this.$router.push("/autenticacion");
+
+      const res = await obtenerDocumentos(token);
+      if(res){
+        this.documentos = res?.data;
+      }
+    },
     adquirir() {
       alert("Opciones de compra...");
-    }
-  }
+    },
+  },
+
+  created() {
+    this.cargarDocumentos();
+  },
 });
 </script>
 
