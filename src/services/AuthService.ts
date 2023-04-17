3ea3bf728e05fe53b77c7e366a2b421704bc1fe9
Jhon Hernandez <jhobahego16@hotmail.com>
2023-04-17 14:45:50 -0500

    servicio para autenticacion creado

---
+++ src/services/AuthService.ts
@@ -0,0 +1,43 @@
+import { AxiosResponse } from 'axios';
+import axios from './Axios'
+import { LoginUser, RegisterUser } from '@/interfaces/User'
+import { Token } from '@/interfaces/Token';
+
+export const registrarUsuario = async (datos: RegisterUser): Promise<AxiosResponse<RegisterUser> | undefined> => {
+  try {
+    const usuarioRegistrado = await axios.post("/usuarios/guardar", datos);
+    return usuarioRegistrado
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export const autenticarUsuario = async (data: LoginUser): Promise<AxiosResponse<Token> | undefined> => {
+  const Form = new FormData();
+  
+  Form.append("username", data.correo);
+  Form.append("password", data.contra);
+
+  try {
+    const respuesta = await axios.post("http://localhost:8000/token", Form, {headers: {
+      'Content-Type': 'application/x-www-form-urlencoded'
+    }});
+    return respuesta;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export const obtenerUsuarioAutenticado = async (token: string): Promise<AxiosResponse<RegisterUser> | undefined> => {
+  try {
+    const respuesta = await axios.get("/usuarios/perfil", {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+
+    return respuesta;
+  } catch (error) {
+    console.log(error);
+  }
+}
