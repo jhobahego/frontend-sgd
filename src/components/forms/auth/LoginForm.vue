27da9cbbe5d5240241bc8791856bf3653f1d59c3
Jhon Hernandez <jhobahego16@hotmail.com>
2023-04-17 14:47:30 -0500

    componente para iniciar sesion

--- src/components/forms/auth/LoginForm.vue
+++ src/components/forms/auth/LoginForm.vue
@@ -1,17 +1,37 @@
 <template>
-  <form action="#" class="form__login">
-    <input class="login__input login__input--first" type="text" placeholder="correo electronico...">
-    <input class="login__input" type="password" placeholder="contraseña...">
-    <button class="btn__login">Iniciar sesión</button>
+  <form class="form__login">
+    <input v-model="credentials.correo" class="login__input login__input--first" type="email" placeholder="correo electronico...">
+    <input v-model="credentials.contra" class="login__input" type="password" placeholder="contraseña...">
+    <button @click.prevent="iniciarSesion()" type="submit" class="btn__login">Iniciar sesión</button>
     <span>¿olvidaste tu contraseña?</span>
   </form>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import { LoginUser } from '@/interfaces/User';
+import { autenticarUsuario } from '@/services/AuthService';
 
 export default defineComponent({
   name: "loginForm",
+
+  data() {
+    return {
+      credentials: {} as LoginUser,
+    }
+  },
+
+  methods: {
+    async iniciarSesion() {
+      const res = await autenticarUsuario(this.credentials);
+      if(res) {
+        localStorage.setItem("token", res.data.access_token);
+        this.$router.push("/");
+      }else{
+        alert("credenciales invalidas");
+      }
+    },
+  },
 })
 </script>
 
