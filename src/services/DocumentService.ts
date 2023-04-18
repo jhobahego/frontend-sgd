842924de7229c895b7a62f4d085f6833267f5b56
Jhon Hernandez <jhobahego16@hotmail.com>
2023-04-17 20:48:39 -0500

    servicio para obtener un documento por su id

--- src/services/DocumentService.ts
+++ src/services/DocumentService.ts
@@ -13,4 +13,17 @@ export const obtenerDocumentos = async (token: string): Promise<AxiosResponse<Do
   } catch (error) {
     console.log(error);
   }
-} 
+}
+
+export const obtenerDocumento = async (documento_id: string | string[], token: string): Promise<AxiosResponse<Document> | undefined> => {
+  try {
+    const documento = await axios.get(`/documentos/${documento_id}`, {
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    });
+    return documento;
+  }catch (error) {
+    console.log(error);
+  }
+}
