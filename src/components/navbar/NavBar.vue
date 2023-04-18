5470f8a302cdfd17d90a1c434ab4b11cf2a34125
Jhon Hernandez <jhobahego16@hotmail.com>
2023-04-17 19:26:17 -0500

    navbar responsive dependiendo de la autenticacion

--- src/components/navbar/NavBar.vue
+++ src/components/navbar/NavBar.vue
@@ -5,33 +5,60 @@
       <button class="navbar__toggle" @click="toggleNav">☰</button>
     </div>
     <ul class="navbar__items" :class="{ 'navbar__items--visible' : isNavVisible }">
-      <li class="navbar__item navbar__item--first">
+      <li class="navbar__item navbar__item--first" v-if="autenticado">
         <router-link class="navbar__link" to="/documentos">Documentos</router-link>
       </li>
       <li class="navbar__item">
         <router-link class="navbar__link" to="/about">Acerca de</router-link>
       </li>
-      <li class="navbar__item">
-        <router-link class="navbar__link" to="/autenticacion">Iniciar sesión</router-link>
-      </li>
+      
+      <router-link class="navbar__btn navbar__btn--link" to="/autenticacion" v-if="!autenticado">Iniciar sesión</router-link>
+      <button class="navbar__btn" @click.prevent="cerrarSesion()" v-if="autenticado">Cerrar sesión</button>
     </ul>
   </nav>
 </template>
 <script lang="ts">
+import { obtenerUsuarioAutenticado } from '@/services/AuthService';
 import { defineComponent } from 'vue';
 
 export default defineComponent({
   name: "showNavegation",
   data() {
     return {
-      isNavVisible: false
+      isNavVisible: false,
+      autenticado: false
     };
   },
+
   methods: {
     toggleNav() {
       this.isNavVisible = !this.isNavVisible;
-    }
-  }
+    },
+
+    async obtenerUsuario() {
+      const token = localStorage.getItem("token");
+      if(token == null) return this.autenticado = false;
+
+      const usuario = await obtenerUsuarioAutenticado(token);
+      if(usuario === undefined) return this.autenticado = false;
+
+      this.autenticado = true;
+    },
+
+    cerrarSesion() {
+      localStorage.removeItem("token");
+      this.autenticado = false;
+      this.$router.push("/autenticacion");
+    },
+  },
+
+  created() {
+    this.obtenerUsuario();
+  },
+
+  watch: {
+    $route: "obtenerUsuario",
+  },
 })
 </script>
 
