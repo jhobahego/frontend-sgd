cc100c11635e703a15ff39ed06494a68e2aafd28
Jhon Hernandez <jhobahego16@hotmail.com>
2023-04-19 13:45:47 -0500

    Formulario de compra estilado

--- src/components/forms/PurchaseForm.vue
+++ src/components/forms/PurchaseForm.vue
@@ -1,26 +1,28 @@
 <template>
   <article class="form__card">
     <figure class="img__container">
-      <img class="form__img" src="@/assets/libro.jpeg" alt="libro the pragmatic programmer" />
+      <img class="document__img" src="@/assets/libro.jpeg" alt="libro the pragmatic programmer" />
     </figure>
 
     <div class="form__container">
-      <header class="form__info">
-        <h3 class="form__title">The pragmatic programmer</h3>
-        <p class="form__description">Quedan 30 unidades</p>
-        <p class="form__description">precio: 30$</p>
+      <header class="document__info">
+        <h3 class="document__title">The pragmatic programmer</h3>
+        <p class="document__description">Quedan 30 unidades</p>
+        <p class="document__description">precio: 30$</p>
       </header>
       <form class="form__body" onsubmit="comprar()">
 
-        <input type="text" placeholder="Correo electronico...">
-        <input type="number" placeholder="Cantidad...">
+        <input class="form__email" type="text" placeholder="Correo electronico...">
+        <input class="form__quantity" type="number" placeholder="Cantidad...">
         <select class="form__select" id="opciones">
           <option value="compra">compra</option>
           <option value="prestamo">prestamo</option>
         </select>
 
-        <input type="submit" class="purchase__btn" value="comprar" />
-        <router-link to="#" class="cancel__btn">Cancelar</router-link>
+        <div class="buttons">
+          <input type="submit" class="purchase__btn" value="comprar" />
+          <router-link to="#" class="cancel__btn">Cancelar</router-link>
+        </div>
       </form>
     </div>
   </article>
