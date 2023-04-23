86553607c464c5ee6e27071c415a97262cf15409
Jhon Hernandez <jhobahego16@hotmail.com>
2023-04-22 21:39:47 -0500

    Archivo de utilidades

---
+++ src/services/Utils.ts
@@ -0,0 +1,54 @@
+import { obtenerUsuarioAutenticado } from "./AuthService";
+import { Solicitud } from "@/interfaces/Solicitud";
+import { Document } from "@/interfaces/Document";
+import { obtenerComprasDeUsuario } from "./Purchase";
+
+export const puedesAdquirir = async ({ correo, opcion, cantidad }: Solicitud, documento: Document): Promise<boolean> => {
+  if(documento.stock < 1) return false;
+  if(opcion === 'prestamo' && cantidad > 1) return false;
+  
+  const correoValido = await verificarCorreo(correo);
+  if(!correoValido) return false;
+  
+  const yaPresto = await haPrestado();
+  if(yaPresto) return false;
+
+  const compraValida = puedeComprar(cantidad, documento.stock);
+  if(opcion === 'compra' && !compraValida) return false;
+  
+  return true;
+}
+
+const verificarCorreo = async (correo: string): Promise<boolean> => {
+  const usuario = await obtenerUsuarioAutenticado();
+  if(!usuario) return false;
+
+  if(correo === usuario.data.correo) return true;
+  
+  return false;
+}
+
+export const obtenerTokenDeLocalStorage = (): string | null => {
+  const token = localStorage.getItem("token");
+  if(token == null) return null;
+
+  return token;
+}
+
+export const haPrestado = async (): Promise<boolean> => {
+  const usuario = await obtenerUsuarioAutenticado();
+  if(!usuario) return false;
+
+  const compras = await obtenerComprasDeUsuario();
+  if(!compras) return false;
+
+  if(compras && usuario.data.nombres === compras.data.nombre_cliente) return true;
+
+  return false;
+}
+
+const puedeComprar = (cantidad: number, stock: number): boolean => {
+  if(cantidad > stock) return false;
+
+  return true;
+}
