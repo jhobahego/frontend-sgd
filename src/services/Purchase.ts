4b5e98283428d3ff5b850ea1be73d6016643e0e7
Jhon Hernandez <jhobahego16@hotmail.com>
2023-04-22 21:40:20 -0500

    Servicio para adquisición de documentos

---
+++ src/services/Purchase.ts
@@ -0,0 +1,56 @@
+import { Document } from "@/interfaces/Document"
+import { Registro } from "@/interfaces/Registro";
+import { Solicitud } from "@/interfaces/Solicitud";
+import { obtenerUsuarioAutenticado } from "./AuthService";
+import { AxiosResponse } from "axios";
+import { obtenerTokenDeLocalStorage, puedesAdquirir } from "./Utils";
+import axios from "./Axios";
+
+export const adquirirDocumento = async (documento: Document, solicitud: Solicitud): Promise<boolean> => {
+  const puedes = await puedesAdquirir(solicitud, documento);
+  if(!puedes) return false;
+
+  const nuevoDocumento = {...documento};
+  nuevoDocumento.stock -= 1;
+
+  const documentoActualizado = await actualizarDocumento(nuevoDocumento);
+  if(!documentoActualizado) return false;
+
+  return true;
+}
+
+export const obtenerComprasDeUsuario = async (): Promise<AxiosResponse<Registro> | undefined> => {
+  const usuario = await obtenerUsuarioAutenticado();
+  if(!usuario) return;
+
+  const token = obtenerTokenDeLocalStorage();
+  if(token == null) return;
+  try{
+    const res = await axios.get(`/ventas/usuario/${usuario.data._id}`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+    return res.data;
+  }catch(error) {
+    return;
+    // console.log(error);
+  }
+}
+
+const actualizarDocumento = async (documento: Document): Promise<AxiosResponse<Document> | undefined> => {
+  const token = obtenerTokenDeLocalStorage();
+  if(token == null) return;
+
+  try {
+    const res = await axios.put(`/documentos/actualizar/${documento._id}`, documento, {
+      headers: {
+        Authorization: `Bearer ${token}`
+      },
+    });
+
+    return res.data;
+  } catch (error) {
+    console.log(`error al actualizar documento: ${error}`);
+  }
+}
