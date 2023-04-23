e414136e970c54122ea97f8831271cccb4d1dfee
Jhon Hernandez <jhobahego16@hotmail.com>
2023-04-22 21:37:15 -0500

    Modelo de registro de adquisiciones

---
+++ src/interfaces/Registro.ts
@@ -0,0 +1,10 @@
+export interface Registro {
+  registro_id: string,
+  id_cliente: string,
+  nombre_cliente: string,
+  id_documento: string,
+  titulo_documento: string,
+  tipo_de_adquisicion: string,
+  cantidad: number,
+  activo: boolean,
+}
