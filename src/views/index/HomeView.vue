7df6e626a79b2b7267a2687306300a86fe113f53
Jhon Hernandez <jhobahego16@hotmail.com>
2023-05-10 20:47:35 -0500

    Fix: notificación eliminada

--- src/views/index/HomeView.vue
+++ src/views/index/HomeView.vue
@@ -1,23 +1,18 @@
 <template>
   <section class="home">
-    <notifications position="top right" animation-type="css" width="400px" :classes="tipoNotificacion"/>
     <header class="title__container" v-if="autenticado">
-      <!-- <h1 class="welcome">Bienvenido {{ username }}</h1> -->
+      <h1 class="welcome">Bienvenido {{ username }}</h1>
       <h2 class="title">Tu galeria de documentos</h2>
     </header>
     <div class="records" v-if="autenticado">
-
-      <!-- <section class="records__container"> -->
-        <article class="record__items" v-for="registro in registros" :key="registro.registro_id">
-          <img :src="registro.imagen" :alt="registro.titulo_documento">
-          <div class="record__info">
-            <h4 class="record__title">{{ registro.titulo_documento }}</h4>
-            <p class="record__description">{{ registro.tipo_de_adquisicion }}</p>
-            <p class="record__description">Documentos adquiridos: {{ registro.cantidad }}</p>
-          </div>
-        </article>
-      <!-- </section> -->
-
+      <article class="record__items" v-for="registro in registros" :key="registro.registro_id">
+        <img :src="registro.imagen" :alt="registro.titulo_documento">
+        <div class="record__info">
+          <h4 class="record__title">{{ registro.titulo_documento }}</h4>
+          <p class="record__description">{{ registro.tipo_de_adquisicion }}</p>
+          <p class="record__description">Documentos adquiridos: {{ registro.cantidad }}</p>
+        </div>
+      </article>
     </div>
     <header class="content" v-if="!autenticado">
       <h1 class="title">Sistema de gestión documental</h1>
@@ -65,13 +60,6 @@ export default defineComponent({
 
       this.autenticado = true;
       this.username = usuario.nombres;
-      this.tipoNotificacion = "notify__topright--success";
-
-      notify({
-        title: "Sesión iniciada",
-        text: `Bienvenido ${this.username}, has iniciado sesión correctamente`,
-        duration: 4000,
-      })
     },
   },
 
