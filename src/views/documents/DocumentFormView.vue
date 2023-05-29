9a28fe52f7a3972d0717207afd16fc1b6f81ec5a
Jhon Hernandez <jhobahego16@hotmail.com>
2023-05-28 20:40:35 -0500

    Feat: formulario para guardar documento

---
+++ src/views/documents/DocumentFormView.vue
@@ -0,0 +1,29 @@
+<template>
+  <section>
+    <h1 class="title">Crea tu documento</h1>
+    <DocumentForm />
+  </section>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import DocumentForm from '@/components/document/DocumentForm.vue';
+
+export default defineComponent({
+  name: "DocumentFormView",
+  components: {
+    DocumentForm,
+  }
+})
+</script>
+
+<style scoped>
+h1 {
+  margin: 1em 0 .4em;
+}
+
+section {
+  margin-left: 4em;
+  margin-right: 4em;
+}
+</style>
