06dec0bf7434633da51cff66c5231c9a049421b0
Jhon Hernandez <jhobahego16@hotmail.com>
2023-06-08 19:40:20 -0500

    Feat: pinia configurado

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,15 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import Notifications from '@kyvg/vue3-notification'
+import { createPinia } from 'pinia'
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 
 const app = createApp(App)
+
+const pinia = createPinia()
+pinia.use(piniaPluginPersistedstate)
+
 app.use(router)
+app.use(pinia)
 app.use(Notifications)
 app.mount('#app')
