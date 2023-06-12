bc16c57f2b39aba01c75c47a71cc459b8dc1dabe
Jhon Hernandez <jhobahego16@hotmail.com>
2023-06-11 20:35:33 -0500

    Style: vista de manejo de usuarios

--- src/views/usuarios/usersView.vue
+++ src/views/usuarios/usersView.vue
@@ -1,7 +1,7 @@
 <template>
   <h1 class="title">Gestión de usuarios</h1>
 
-  <h3 class="users__title">Usuarios registrados</h3>
+  <h2 class="users__title">Usuarios registrados</h2>
   <ul class="users__list">
     <li class="user__item" v-for="usuario in usuarios" :key="usuario._id">
       <h4 class="user__names">{{ usuario.nombres }} {{ usuario.apellidos }}</h4>
@@ -24,32 +24,5 @@ onMounted(async () => {
 </script>
 
 <style scoped>
-.title {
-  color: blue;
-  font-weight: 600;
-  text-align: center;
-  margin-bottom: 1.2em;
-}
-
-.users__title {
-  text-align: center;
-  color: blue;
-  margin-bottom: .6em;
-}
-
-.users__list {
-  width: 100%;
-  max-width: 400px;
-  /* background: var(--light-blue-alpha); */
-  margin: 0 auto;
-}
-
-.user__item {
-  width: 100%;
-  text-align: center;
-  text-decoration: none;
-  list-style: none;
-  padding: .6em 0;
-  /* border-bottom: 1px solid #0008; */
-}
+@import url(usersView.css);
 </style>
