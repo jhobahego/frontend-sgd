cd99ab0136d2800890f5f64ba18bd548d44058a2
Jhon Hernandez <jhobahego16@hotmail.com>
2023-06-15 20:05:42 -0500

    Feat: vista de admin para gestionar documentos

---
+++ src/views/admin/AllDocumentsView.vue
@@ -0,0 +1,23 @@
+<template>
+  <h1 class="title">Gestionar documentos</h1>
+  <ul class="documents__info">
+    <li class="document__item" v-for="documento in documentos" :key="documento._id">
+      <router-link class="document__title" :to="{ name: 'DocumentDetailAdmin', params: { id: documento._id } }">{{ documento.titulo }}</router-link>
+    </li>
+  </ul>
+</template>
+
+<script lang="ts" setup>
+import { Ref, ref, onMounted } from 'vue';
+import { Documento } from '@/interfaces/Documento';
+import { obtenerDocumentos } from '@/services/DocumentService';
+
+const documentos: Ref<Documento[]> = ref([]);
+
+onMounted(async () => {
+  documentos.value = await obtenerDocumentos();
+})
+</script>
+
+<style scoped>
+</style>
