8be642b62796f2468e12f50f060708939aab7524
Jhon Hernandez <jhobahego16@hotmail.com>
2023-06-15 20:57:27 -0500

    Feat: actualizar documento por admin

--- src/services/DocumentService.ts
+++ src/services/DocumentService.ts
@@ -2,6 +2,9 @@ import { Documento } from "@/interfaces/Documento";
 import { obtenerTokenDeLocalStorage } from "./Utils";
 import { notify } from '@kyvg/vue3-notification';
 import axiosInstance from './Axios'
+import { useAuth } from "@/store/authStore";
+import { AxiosError } from "axios";
+import { ApiErrorMessage, UpdateResponse } from "@/types";
 
 export const obtenerDocumentos = async (): Promise<Documento[]> => {
   const token = obtenerTokenDeLocalStorage();
@@ -35,17 +38,17 @@ export const obtenerDocumento = async (documento_id: string | string[]): Promise
 export const guardarDocumentoEnBD = async (form: FormData): Promise<Documento> => {
   try {
     const token = obtenerTokenDeLocalStorage();
-    
+
     const res = await axiosInstance.post("/documentos/guardar", form, {
       headers: {
         "Content-Type": "multipart/form-data",
         Authorization: `Bearer ${token}`
       }
     });
-    if(res){
+    if (res) {
       return res.data;
     }
-  } catch(e) {
+  } catch (e) {
     notify({
       title: "Error al guardar",
       text: "No se pudo guardar el documento",
@@ -54,4 +57,27 @@ export const guardarDocumentoEnBD = async (form: FormData): Promise<Documento> =
     })
   }
   return {} as Documento;
+}
+
+export async function actualizarDocumento(documento: Documento): Promise<UpdateResponse> {
+  const authStore = useAuth();
+  const token = authStore.token;
+
+  try {
+    const respuesta = await axiosInstance.put(`/documentos/actualizar/${documento._id}`, documento, {
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    })
+
+    return { body: respuesta.data, message: "" } as UpdateResponse;
+  } catch (error) {
+    const status = (error as AxiosError).response?.status;
+    if (status === 401 || status === 404) {
+      const apiError = (error as AxiosError).response?.data as ApiErrorMessage;
+      return { message: apiError.detail } as UpdateResponse;
+    }
+
+    return { message: "Error en la red, intentelo mas tarde" } as UpdateResponse;
+  }
 }
---
+++ src/views/admin/DocumentDetailView.vue
@@ -0,0 +1,92 @@
+<template>
+  <h1>gestionar documento</h1>
+  <form>
+    <label class="label__titulo">
+      titulo
+      <input type="text" class="input__titulo" placeholder="clean code" v-model="documento.titulo">
+    </label>
+
+    <div class="div__joiner">
+      <label type="text" class="label__stock">
+        stock
+        <input type="number" class="input__stock" placeholder="24" v-model="documento.stock">
+      </label>
+      <label type="number" class="label__precio">
+        precio
+        <input type="text" class="input__precio" placeholder="64" v-model="documento.precio">
+      </label>
+    </div>
+
+    <div class="div__joiner">
+      <label>
+        tipo
+        <select v-model="documento.tipo_documento">
+          <option value="fisico">fisico</option>
+          <option value="digital">digital</option>
+        </select>
+      </label>
+
+      <label>
+        categoria
+        <select v-model="documento.categoria">
+          <option value="desarrollo de software">desarrollo de software</option>
+          <option value="romantico">romantico</option>
+          <option value="contaduria">contaduria</option>
+          <option value="deportes">deportes</option>
+          <option value="comics">comics</option>
+          <option value="anime">anime</option>
+        </select>
+      </label>
+    </div>
+
+    <label class="label__editorial">
+      editorial
+      <input type="text" class="input__editorial" placeholder="bogota sas, london-edits..." v-model="documento.editorial">
+    </label>
+
+    <div class="buttons__container">
+      <button @click.prevent="editarDocumento(documento)">Actualizar</button>
+      <router-link :to="{ name: 'AllDocumentsAdmin' }">Volver</router-link>
+    </div>
+  </form>
+  <notifications position="bottom right" animation-type="css" width="50vw" />
+</template>
+
+<script lang="ts" setup>
+import { Ref, ref, onMounted } from 'vue'
+import { useRoute } from 'vue-router';
+import { Documento } from '@/interfaces/Documento';
+import { obtenerDocumento, actualizarDocumento } from '@/services/DocumentService';
+import { notify } from '@kyvg/vue3-notification';
+
+const documento: Ref<Documento> = ref({} as Documento);
+
+onMounted(async () => {
+  const route = useRoute();
+  const id = route.params.id;
+
+  documento.value = await obtenerDocumento(id);
+})
+
+async function editarDocumento(documento: Documento) {
+  const respuesta = await actualizarDocumento(documento);
+
+  if (respuesta.message.length > 0) {
+    notify({
+      title: "Fallo al editar",
+      text: `${respuesta.message}`,
+      type: "error",
+      duration: 3000,
+    })
+    return;
+  }
+
+  notify({
+    title: "Documento actualizado",
+    text: `Documento con titulo ${respuesta.body.titulo} actualizado correctamente`,
+    type: "success",
+    duration: 3000,
+  })
+}
+
+</script>
