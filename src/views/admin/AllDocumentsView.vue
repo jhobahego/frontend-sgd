59cd633a5808484043ba3a3ca157a2c794f3004a
Jhon Hernandez <jhobahego16@hotmail.com>
2023-06-20 20:43:23 -0500

    Style: cabecera de gestión de documentos

--- src/views/admin/AllDocumentsView.vue
+++ src/views/admin/AllDocumentsView.vue
@@ -2,9 +2,10 @@
   <header class="documents__manage">
     <h1 class="title">Gestionar documentos</h1>
     <form class="documentsCreate__form">
+      <router-link class="createDocument__btn" :to="{ name: 'documentFormView' }">Crear documento</router-link>
       <label class="documents__searchlabel">
-        buscar por
-        <select class="documents__searchtype" v-model="tipoDeBusqueda">
+        Filtrar por
+        <select class="type__select" v-model="tipoDeBusqueda">
           <option value="titulo">titulo</option>
           <option value="categoria">categoria</option>
           <!-- <option value="precio">precio</option> -->
@@ -12,12 +13,10 @@
         </select>
       </label>
       <label class="documents__searchlabel">
-        buscar
+        Buscar
         <input type="text" class="documents__searchinput" placeholder="clean code, head first java..." v-model="busqueda">
       </label>
-      <button class="search__btn">buscar</button>
     </form>
-    <router-link class="createDocument__btn" :to="{ name: 'documentFormView' }">Crear documento</router-link>
   </header>
 
   <h3 class="list__title">Listado de documentos</h3>
