099cb379d84bb807e43aa908973c54947b50ebb6
Jhon Hernandez <jhobahego16@hotmail.com>
2023-06-22 21:43:39 -0500

    Feat: filtrado por titulo, idioma o categoria

--- src/views/admin/AllDocumentsView.vue
+++ src/views/admin/AllDocumentsView.vue
@@ -12,16 +12,38 @@
           <option value="idioma">idioma</option>
         </select>
       </label>
-      <label class="documents__searchlabel">
+
+      <label class="documents__searchlabel" v-if="categoria">
+        categoria:
+        <select class="documents__searchinput" v-model="busqueda">
+          <option value="desarrollo de software">desarrollo de software</option>
+          <option value="deportes">deportes</option>
+          <option value="contaduria">contaduria</option>
+          <option value="romantico">romantico</option>
+          <option value="comics">comics</option>
+          <option value="anime">anime</option>
+        </select>
+      </label>
+
+      <label class="documents__searchlabel" v-else-if="idioma">
+        idioma:
+        <select class="documents__searchinput" v-model="busqueda">
+          <option value="ingles">ingles</option>
+          <option value="español">español</option>
+        </select>
+      </label>
+
+      <label class="documents__searchlabel" v-else>
         Buscar
         <input type="text" class="documents__searchinput" placeholder="clean code, head first java..." v-model="busqueda">
       </label>
     </form>
   </header>
 
-  <h3 class="list__title">Listado de documentos</h3>
+  <h3 v-show="documentosFiltrados.length > 0" class="list__title">{{ mensaje }}</h3>
+  <h3 v-show="documentosFiltrados.length === 0" class="list__title">{{ mensaje }}</h3>
   <ul class="documents__info">
-    <li class="document__item" v-for="documento in documentos" :key="documento._id">
+    <li class="document__item" v-for="documento in documentosFiltrados" :key="documento._id">
       <router-link class="document__title" :to="{ name: 'DocumentDetailAdmin', params: { id: documento._id } }">{{
         documento.titulo }}</router-link>
     </li>
@@ -29,7 +51,7 @@
 </template>
 
 <script lang="ts" setup>
-import { Ref, ref, onMounted } from 'vue';
+import { Ref, ref, onMounted, computed, watch } from 'vue';
 import { Documento } from '@/interfaces/Documento';
 import { obtenerDocumentos } from '@/services/DocumentService';
 
@@ -37,8 +59,57 @@ const documentos: Ref<Documento[]> = ref([]);
 const busqueda: Ref<string> = ref('');
 const tipoDeBusqueda: Ref<string> = ref('titulo');
 
+const idioma = computed(() => {
+  return tipoDeBusqueda.value === "idioma";
+})
+
+const categoria = computed(() => {
+  return tipoDeBusqueda.value === "categoria";
+})
+
+const documentosFiltrados: Ref<Documento[]> = ref([]);
+
+const mensaje = computed(() => {
+  if (documentosFiltrados.value.length === 0) {
+    return "No se han encontrado documentos";
+  }
+  return "Listado de documentos";
+})
+
+function seleccionarCriterio(): void {
+  const tipo = tipoDeBusqueda.value;
+  if (tipo === "idioma") {
+    busqueda.value = "español";
+  } else if (tipo === "categoria") {
+    busqueda.value = "desarrollo de software";
+  } else {
+    busqueda.value = "";
+  }
+}
+
+function filtro() {
+  documentosFiltrados.value = documentos.value;
+
+  const tipo = tipoDeBusqueda.value;
+  if (tipo === "idioma") {
+    documentosFiltrados.value = documentos.value.filter((documento) => documento.idioma === busqueda.value);
+  } else if (tipo === "categoria") {
+    documentosFiltrados.value = documentos.value.filter((documento) => documento.categoria === busqueda.value);
+  } else {
+    documentosFiltrados.value = documentos.value.filter((documento) => documento.titulo.toLowerCase().includes(busqueda.value.toLowerCase()));
+  }
+
+  if (documentos.value.length === 0) {
+    console.log({ message: "No se han encontrado documentos" });
+  }
+}
+
+watch(tipoDeBusqueda, seleccionarCriterio);
+watch(busqueda, filtro);
+
 onMounted(async () => {
   documentos.value = await obtenerDocumentos();
+  documentosFiltrados.value = documentos.value;
 })
 </script>
 
