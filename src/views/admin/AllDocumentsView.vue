cdab4db57e6d3221513435309fb2ce80f04237cd
Jhon Hernandez <jhobahego16@hotmail.com>
2023-06-24 21:26:21 -0500

    Feat: filtro de documentos

--- src/views/admin/AllDocumentsView.vue
+++ src/views/admin/AllDocumentsView.vue
@@ -8,109 +8,104 @@
         <select class="type__select" v-model="tipoDeBusqueda">
           <option value="titulo">titulo</option>
           <option value="categoria">categoria</option>
-          <!-- <option value="precio">precio</option> -->
           <option value="idioma">idioma</option>
         </select>
       </label>
 
-      <label class="documents__searchlabel" v-if="categoria">
-        categoria:
-        <select class="documents__searchinput" v-model="busqueda">
-          <option value="desarrollo de software">desarrollo de software</option>
-          <option value="deportes">deportes</option>
-          <option value="contaduria">contaduria</option>
-          <option value="romantico">romantico</option>
-          <option value="comics">comics</option>
-          <option value="anime">anime</option>
-        </select>
-      </label>
+      <template v-if="categoria">
+        <label class="documents__searchlabel">
+          categoria:
+          <select class="documents__searchinput" v-model="busqueda">
+            <option value="desarrollo de software">desarrollo de software</option>
+            <option value="deportes">deportes</option>
+            <option value="contaduria">contaduria</option>
+            <option value="romantico">romantico</option>
+            <option value="comics">comics</option>
+            <option value="anime">anime</option>
+          </select>
+        </label>
+      </template>
 
-      <label class="documents__searchlabel" v-else-if="idioma">
-        idioma:
-        <select class="documents__searchinput" v-model="busqueda">
-          <option value="ingles">ingles</option>
-          <option value="español">español</option>
-        </select>
-      </label>
+      <template v-else-if="idioma">
+        <label class="documents__searchlabel">
+          idioma:
+          <select class="documents__searchinput" v-model="busqueda">
+            <option value="ingles">ingles</option>
+            <option value="español">español</option>
+          </select>
+        </label>
+      </template>
 
-      <label class="documents__searchlabel" v-else>
-        Buscar
-        <input type="text" class="documents__searchinput" placeholder="clean code, head first java..." v-model="busqueda">
-      </label>
+      <template v-else>
+        <label class="documents__searchlabel">
+          Buscar
+          <input type="text" class="documents__searchinput" placeholder="clean code, head first java..." v-model="busqueda">
+        </label>
+      </template>
     </form>
   </header>
 
-  <h3 v-show="documentosFiltrados.length > 0" class="list__title">{{ mensaje }}</h3>
-  <h3 v-show="documentosFiltrados.length === 0" class="list__title">{{ mensaje }}</h3>
+  <h3 class="list__title">{{ mensaje }}</h3>
   <ul class="documents__info">
     <li class="document__item" v-for="documento in documentosFiltrados" :key="documento._id">
-      <router-link class="document__title" :to="{ name: 'DocumentDetailAdmin', params: { id: documento._id } }">{{
-        documento.titulo }}</router-link>
+      <router-link class="document__title" :to="{ name: 'DocumentDetailAdmin', params: { id: documento._id } }">{{ documento.titulo }}</router-link>
     </li>
   </ul>
 </template>
 
 <script lang="ts" setup>
-import { Ref, ref, onMounted, computed, watch } from 'vue';
+import { ref, onMounted, computed, watch } from 'vue';
 import { Documento } from '@/interfaces/Documento';
 import { obtenerDocumentos } from '@/services/DocumentService';
 
-const documentos: Ref<Documento[]> = ref([]);
-const busqueda: Ref<string> = ref('');
-const tipoDeBusqueda: Ref<string> = ref('titulo');
+const documentos = ref<Documento[]>([]);
+const busqueda = ref('');
+const tipoDeBusqueda = ref('titulo');
 
-const idioma = computed(() => {
-  return tipoDeBusqueda.value === "idioma";
-})
+const idioma = computed(() => tipoDeBusqueda.value === 'idioma');
+const categoria = computed(() => tipoDeBusqueda.value === 'categoria');
 
-const categoria = computed(() => {
-  return tipoDeBusqueda.value === "categoria";
-})
-
-const documentosFiltrados: Ref<Documento[]> = ref([]);
+const documentosFiltrados = ref<Documento[]>([]);
 
 const mensaje = computed(() => {
   if (documentosFiltrados.value.length === 0) {
-    return "No se han encontrado documentos";
+    return 'No se han encontrado documentos';
   }
-  return "Listado de documentos";
-})
-
-function seleccionarCriterio(): void {
-  const tipo = tipoDeBusqueda.value;
-  if (tipo === "idioma") {
-    busqueda.value = "español";
-  } else if (tipo === "categoria") {
-    busqueda.value = "desarrollo de software";
-  } else {
-    busqueda.value = "";
-  }
-}
+  return 'Listado de documentos';
+});
 
 function filtro() {
-  documentosFiltrados.value = documentos.value;
-
   const tipo = tipoDeBusqueda.value;
-  if (tipo === "idioma") {
+
+  if (tipo === 'idioma') {
     documentosFiltrados.value = documentos.value.filter((documento) => documento.idioma === busqueda.value);
-  } else if (tipo === "categoria") {
+  } else if (tipo === 'categoria') {
     documentosFiltrados.value = documentos.value.filter((documento) => documento.categoria === busqueda.value);
   } else {
     documentosFiltrados.value = documentos.value.filter((documento) => documento.titulo.toLowerCase().includes(busqueda.value.toLowerCase()));
   }
 
   if (documentos.value.length === 0) {
-    console.log({ message: "No se han encontrado documentos" });
+    console.log({ message: 'No se han encontrado documentos' });
   }
 }
 
-watch(tipoDeBusqueda, seleccionarCriterio);
-watch(busqueda, filtro);
+watch(tipoDeBusqueda, () => {
+  if (tipoDeBusqueda.value === 'idioma') {
+    busqueda.value = 'español';
+  } else if (tipoDeBusqueda.value === 'categoria') {
+    busqueda.value = 'desarrollo de software';
+  } else {
+    busqueda.value = '';
+  }
+});
+
+watch([busqueda, tipoDeBusqueda], filtro);
 
 onMounted(async () => {
   documentos.value = await obtenerDocumentos();
   documentosFiltrados.value = documentos.value;
-})
+});
 </script>
 
 <style scoped>
