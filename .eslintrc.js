6fa07f622aaa7330d499e95fd5f59c95958ad930
Jhon Hernandez <jhobahego16@hotmail.com>
2023-07-12 18:28:58 -0500

    Feat: proyecto pasado a vite

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,7 @@
-module.exports = {
+export default {
   root: true,
   env: {
-    node: true
+    es2021: true
   },
   'extends': [
     'plugin:vue/vue3-essential',
@@ -9,10 +9,10 @@ module.exports = {
     '@vue/typescript/recommended'
   ],
   parserOptions: {
-    ecmaVersion: 2020
+    ecmaVersion: 2021
   },
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-console': import.meta.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': import.meta.env.NODE_ENV === 'production' ? 'warn' : 'off'
   }
 }
--- src/components/botones/SubmitBtn.vue
+++ src/components/botones/SubmitBtn.vue
@@ -3,7 +3,7 @@
 </template>
 
 <script lang="ts" setup>
-  import { defineProps } from 'vue'
+  // import { defineProps } from 'vue';
 
   const iniciarSesion = () => {
     props.action();
