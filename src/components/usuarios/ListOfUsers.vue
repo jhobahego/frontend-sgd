e2c9de48f10dba10ac65acc4a3f7432ed534fe4c
Jhon Hernandez <jhobahego16@hotmail.com>
2023-07-11 21:13:17 -0500

    Feat: boton para ver mas detalles del usuario

--- src/components/usuarios/ListOfUsers.vue
+++ src/components/usuarios/ListOfUsers.vue
@@ -7,13 +7,13 @@
     </header>
     <ul class="userslist" v-for="usuario in usuariosLimitados" :key="usuario._id">
       <li class="userslist__item">
-        <p class="userslist__detail">
+        <p class="userslist__nombres">
           <strong>Nombres: <span class="userslist__data">{{ usuario.nombres }}</span></strong>
         </p>
-
         <p class="userslist__detail">
           <strong>Correo: <span class="userslist__data">{{ usuario.correo }}</span></strong>
         </p>
+        <button class="user__details" @click="verDetalle(usuario._id)">Ver mas detalles</button>
       </li>
     </ul>
   </article>
@@ -22,6 +22,7 @@
 <script lang="ts">
 import { defineComponent, computed } from 'vue';
 import { UserResponse } from '../../interfaces/User'
+import { useRouter } from 'vue-router';
 
 export default defineComponent({
   props: {
@@ -39,6 +40,13 @@ export default defineComponent({
     }
   },
   setup(props) {
+    const router = useRouter();
+
+    const verDetalle = (usuario_id: string | undefined) => {
+      if (usuario_id != undefined) {
+        router.push({ name: 'EditarUsuario', params: { id: usuario_id} })
+      }
+    }
     const usuariosLimitados = computed(() => {
       if (props.limite && props.usuarios.length > props.limite) {
         return props.usuarios.slice(0, props.limite);
@@ -48,7 +56,8 @@ export default defineComponent({
     });
 
     return {
-      usuariosLimitados
+      usuariosLimitados,
+      verDetalle
     };
   }
 })
