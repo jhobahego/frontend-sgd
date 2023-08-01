2cf0eea38c071b987b5bbecec16218af790af162
Jhon Hernandez <jhobahego16@hotmail.com>
2023-07-31 21:36:17 -0500

    Refactor: status code controlado

--- src/services/Axios.ts
+++ src/services/Axios.ts
@@ -27,7 +27,7 @@ axiosInstance.interceptors.request.use((request: InternalAxiosRequestConfig) => 
 axiosInstance.interceptors.response.use(
   (response: AxiosResponse) => {
     const { status } = response
-    if (status === 200) return response;
+    if (status === 200 || status === 204) return response;
 
     notificationUtilities.success(getResponseMessage(status));
     return response
