073a51acd27256f1a972baa55319bf00b5c47dad
Jhon Hernandez <jhobahego16@hotmail.com>
2023-07-31 20:51:10 -0500

    Refactor: servicio de autenticacion

--- src/services/AuthService.ts
+++ src/services/AuthService.ts
@@ -1,45 +1,28 @@
 import { LoginUser, RegisterUser, UserResponse } from '@/interfaces/User'
 import { Token } from '@/interfaces/Token'
 import axiosInstance from './Axios';
+import { AxiosResponse } from 'axios';
 
-export const registrarUsuario = async (datos: RegisterUser): Promise<UserResponse> => {
-  try {
-    const respuesta = await axiosInstance.post("/usuarios/guardar", datos);
-    return respuesta.data as UserResponse;
-  } catch (error) {
-    console.log(error);
-    return {} as UserResponse;
-  }
+export const registrarUsuario = async (datos: RegisterUser): Promise<AxiosResponse<UserResponse>> => {
+  return await axiosInstance.post("/usuarios/guardar", datos);
 }
 
-export const autenticarUsuario = async (data: LoginUser): Promise<Token | undefined> => {
+export const autenticarUsuario = async (data: LoginUser): Promise<AxiosResponse<Token>> => {
   const Form = new FormData();
 
   Form.append("username", data.correo);
   Form.append("password", data.contra);
 
-  try {
-    const respuesta = await axiosInstance.post("/token", Form, {
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded'
-      }
-    });
 
-    return respuesta.data;
-  } catch (error) {
-    console.log(error);
-  }
+  const response = await axiosInstance.post("/token", Form, {
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded'
+    }
+  });
+
+  return response
 }
 
-export const obtenerUsuarioAutenticado = async (token: string | null): Promise<UserResponse> => {
-  if (token?.length === 0) return {} as UserResponse;
-
-  try {
-    const respuesta = await axiosInstance.get("/usuarios/perfil")
-
-    return respuesta.data;
-  } catch (error) {
-    console.log(`error al obtener usuario autenticado: ${error}`);
-    return {} as UserResponse;
-  }
+export const obtenerUsuarioAutenticado = async (): Promise<AxiosResponse<UserResponse>> => {
+  return await axiosInstance.get("/usuarios/perfil")
 }
