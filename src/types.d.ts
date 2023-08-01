fa021926692a75ff2c3c4b86ee005c346471ea0c
Jhon Hernandez <jhobahego16@hotmail.com>
2023-07-31 21:39:32 -0500

    Refactor: tipos innecesarios

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,18 +1,4 @@
 import { Documento } from "./interfaces/Documento"
-export type ApiErrorMessage = { detail: string }
-
-type Body = RegisterUser | Documento
-
-export interface ApiResponse {
-  success: boolean,
-  body: Body,
-  message: string
-}
-
-export interface DeleteResponse {
-  status: boolean,
-  message: string
-}
 
 export interface PurchaseValidationResult {
   canBuy: boolean = false,
