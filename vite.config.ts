2176d51bd609943c9391a75f5178b8657cccc261
Jhon Hernandez <jhobahego16@hotmail.com>
2025-03-11 00:52:11 -0500

    Fix: Build de aplicación arreglando problemas de dependencias, tipos de Typescript y archivos de configuración

--- vite.config.ts
+++ vite.config.ts
@@ -19,6 +19,19 @@ export default defineConfig(({ mode }) => {
     // Puedes exponer las variables de entorno aquí si necesitas más control
     define: {
       'process.env': env
-    }
+    },
+    build: {
+      outDir: 'dist',
+      assetsDir: 'assets',
+      sourcemap: true,
+      // minify: 'terser',
+      // terserOptions: {
+      //   compress: {
+      //     drop_console: true,
+      //     drop_debugger: true
+      //   }              
+      // },
+      // chunkSizeWarningLimit: 2000
+    },
   }
 })
